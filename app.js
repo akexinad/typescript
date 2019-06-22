84b3ecda0985561b5c04cabf4b27692f3748b609
akexinad <danixeka@gmail.com>
2019-06-22 10:23:35 +1000

    typescript types - arrays, tuples, enums

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 
 TYPES IN TYPESCRIPT
@@ -5,28 +6,21 @@ TYPES IN TYPESCRIPT
 - Typescript obviously has all your usual types. AND THEN SOME!
 
 */
-
 // String
-let myName = "Fellini";
+var myName = "Fellini";
 // myName = 27; => ERROR
-
-
 // Just remember with arrays that if you want an array that holds any type you have to declare it as follows:
-
-const arrayOfAny: any[] = ["fellini", 24, true];
-
+var arrayOfAny = ["fellini", 24, true];
 // TUPLES
 // These are mixed arrays of many types, but you can customize its structure by declaring what types are where and how many items.
-const tupleArray: [string, number, boolean] = ["Fellini", 44, true];
-
+var tupleArray = ["Fellini", 44, true];
 // ENUMS
 // Enums allow us to define a set of named constants. Using enums can make it easier to document intent, or create a set of distinct cases. TypeScript provides both numeric and string-based enums.
-
-enum Color {
-    blue, // => will equal to 0
-    green = 100,
-    red // => 101
-}
-
-let myColor = Color.blue;
+var Color;
+(function (Color) {
+    Color[Color["blue"] = 0] = "blue";
+    Color[Color["green"] = 100] = "green";
+    Color[Color["red"] = 101] = "red"; // => 101
+})(Color || (Color = {}));
+var myColor = Color.blue;
 console.log(myColor);
