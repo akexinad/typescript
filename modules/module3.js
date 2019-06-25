a895bcee85a51190452cb80a98571d8b851ab351
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-25 16:14:52 +1000

    Inheritance, constructors, getters and setters

--- modules/module3.js
+++ modules/module3.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // This is the conventional way of declaring variables in TS.
 var Person = /** @class */ (function () {
     function Person(name, username) {
@@ -13,7 +26,7 @@ console.log(person.username);
 // THIS IS SHORTER SYNTAX FOR DECLARING CLASS METHODS AND VARIABLES.
 var Car = /** @class */ (function () {
     function Car(make, model, designer) {
-        if (designer === void 0) { designer = 'pininfarina'; }
+        if (designer === void 0) { designer = 'Pininfarina'; }
         this.make = make;
         this.model = model;
         this.designer = designer;
@@ -30,3 +43,38 @@ var Car = /** @class */ (function () {
 var myCar = new Car('Ferrari', 'F40');
 console.log(myCar.printModel());
 console.log(myCar);
+// INHERITANCE
+var Ferrari = /** @class */ (function (_super) {
+    __extends(Ferrari, _super);
+    function Ferrari(model) {
+        return _super.call(this, "Ferrari", model) || this;
+    }
+    return Ferrari;
+}(Car));
+var myOtherCar = new Ferrari("F40");
+// GETTERS AND SETTERS
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this._species = "Default";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = "Default";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+console.log(plant.species);
+plant.species = "Rose";
+console.log(plant.species);
--- modules/module3.ts
+++ modules/module3.ts
@@ -20,7 +20,7 @@ class Car {
     constructor(
         public make: string,
         protected model: string,
-        private designer: string = 'pininfarina'
+        private designer: string = 'Pininfarina'
     ) { }
 
     printModel() {
@@ -37,4 +37,45 @@ const myCar = new Car('Ferrari', 'F40');
 
 console.log(myCar.printModel());
 console.log(myCar);
+
+
+
+
+// INHERITANCE
+
+class Ferrari extends Car {
+
+    constructor(model: string) {
+        super("Ferrari", model);
+    }
+}
+
+const myOtherCar = new Ferrari("F40");
+
+
+
+
+// GETTERS AND SETTERS
+
+class Plant {
+    private _species: string = "Default"
+
+    get species() {
+        return this._species;
+    }
+
+    set species(value: string) {
+        if (value.length > 3) {
+            this._species = value;
+        } else {
+            this._species = "Default";
+        }
+    }
+}
+
+let plant = new Plant();
+console.log(plant.species);
+plant.species = "Rose";
+console.log(plant.species);
+
 
