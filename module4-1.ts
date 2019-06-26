71334bb753bb185cc45d8962dc7a754f80d781b6
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-26 13:33:59 +1000

    79. Importing & Exporting Modules

--- module4-1.ts
+++ module4-1.ts
@@ -1,5 +1,6 @@
-import { PI, calculateCircumference } from './math/circle';
+import * as Circle from './math/circle';
+import calculateRectangle from './math/rectangle';
 
-console.log(PI);
-console.log(calculateCircumference(10));
-// console.log();
+console.log(Circle.PI);
+console.log(Circle.calculateCircumference(10));
+console.log(calculateRectangle(20, 50));
