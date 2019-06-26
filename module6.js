4e83efa640d110ecf0947b2bb0beb1ba7ac90566
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-27 08:33:30 +1000

    97. Using Generic Types

--- module6.js
+++ module6.js
@@ -16,7 +16,7 @@ function betterEcho(data) {
     return data;
 }
 console.log(betterEcho("Max"));
-console.log(betterEcho(27).length); // => This gives a more helpful error if you looke into the console and the IDE is also aware.
+// console.log(betterEcho<number>(27).length); // => This gives a more helpful error if you looke into the console and the IDE is also aware.
 console.log(betterEcho({
     name: "Max",
     age: 27
@@ -26,3 +26,14 @@ var testResults = [45, 65];
 // NOTE that this is the same as => const testResults: number[] = [45, 65];
 testResults.push(33);
 // testResults.push('yes'); // => error
+// ARRAYS
+function printAll(args) {
+    args.forEach(function (el) { return console.log(el); });
+}
+printAll(['fellini', 'benigni', 'pasolini']);
+// The equivalent generic type of the above function would be as follows.
+// So, what the expression below says is that:
+// echo is a function where the input is of type whoa, and it returns something which is of type Whoa.
+var echo2 = betterEcho;
+console.log(echo2("Something"));
+// console.log(echo2<string>(22)); // => Argument of type '22' is not assignable to parameter of type 'string'.ts(2345)
--- module6.ts
+++ module6.ts
@@ -23,7 +23,7 @@ function betterEcho<T>(data: T) {
 }
 
 console.log(betterEcho("Max"));
-console.log(betterEcho<number>(27).length); // => This gives a more helpful error if you looke into the console and the IDE is also aware.
+// console.log(betterEcho<number>(27).length); // => This gives a more helpful error if you looke into the console and the IDE is also aware.
 console.log(betterEcho({
     name: "Max",
     age: 27
@@ -38,3 +38,22 @@ const testResults: Array<number> = [45, 65];
 // NOTE that this is the same as => const testResults: number[] = [45, 65];
 testResults.push(33);
 // testResults.push('yes'); // => error
+
+
+
+
+// ARRAYS
+
+function printAll<T>(args: T[]) {
+    args.forEach( el => console.log(el));
+}
+
+printAll(['fellini', 'benigni', 'pasolini']);
+
+// The equivalent generic type of the above function would be as follows.
+// So, what the expression below says is that:
+// echo is a function where the input is of type whoa, and it returns something which is of type Whoa.
+const echo2: <Whoa>(data: Whoa) => Whoa = betterEcho;
+
+console.log(echo2<string>("Something"));
+// console.log(echo2<string>(22)); // => Argument of type '22' is not assignable to parameter of type 'string'.ts(2345)
