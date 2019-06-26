d123bd4ed9fd107b94b0234a5dce1efc098f2975
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-06-26 15:27:06 +1000

    87. Using Interfaces with Classes

--- module5.js
+++ module5.js
@@ -14,6 +14,7 @@ function greet(person) {
 function changeName(person) {
     person.firstName = "Anna";
 }
+// Strict checks only happen when you pass in an object literal.
 // greet({
 //     firstName: "Max",
 //     // age: 27
@@ -21,3 +22,16 @@ function changeName(person) {
 // });
 changeName(person);
 person.greet('Lopez');
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi my name is " + this.firstName + " " + lastName);
+    };
+    ;
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = "Federico";
+greet(myPerson);
+myPerson.greet("Fellini");
--- module5.ts
+++ module5.ts
@@ -28,6 +28,8 @@ function changeName(person: NamedPerson) {
     person.firstName = "Anna";
 }
 
+// Strict checks only happen when you pass in an object literal.
+
 // greet({
 //     firstName: "Max",
 //     // age: 27
@@ -35,3 +37,16 @@ function changeName(person: NamedPerson) {
 // });
 changeName(person);
 person.greet('Lopez');
+
+class Person implements NamedPerson {
+    firstName: string;
+    greet(lastName: string) {
+        console.log(`Hi my name is ${ this.firstName } ${ lastName }`);        
+    };
+}
+
+const myPerson = new Person();
+myPerson.firstName = "Federico";
+greet(myPerson);
+myPerson.greet("Fellini");
+
